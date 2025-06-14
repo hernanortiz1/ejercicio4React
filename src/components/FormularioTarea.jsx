4e06a3ec93071eb1e6554ae10b56f26b326cca17
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-06-14 16:05:05 -0300

    agregue maquetado de ListaTareas y de ItemTarea

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,15 +1,17 @@
 import {Form, Button} from "react-bootstrap";
+import ListaTareas from "./ListaTareas";
 
 const FormularioTarea = () => {
   return (
-    <div>
+    <section>
       <Form>
         <Form.Group className="mb-3 d-flex">
           <Form.Control type="text" placeholder="Ingresa una tarea" />
         <Button variant="info" className="ms-3">Enviar</Button>
         </Form.Group>
       </Form>
-    </div>
+      <ListaTareas/>
+    </section>
   );
 };
 
