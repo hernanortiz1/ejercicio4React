77199178bf0474a4c0de1404a2505331f845e85d
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-06-14 15:38:23 -0300

    agregue maquetado de componente Formulario

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,9 +1,16 @@
+import {Form, Button} from "react-bootstrap";
+
 const FormularioTarea = () => {
-    return (
-        <div>
-                        
-        </div>
-    );
+  return (
+    <div>
+      <Form>
+        <Form.Group className="mb-3 d-flex">
+          <Form.Control type="text" placeholder="Ingresa una tarea" />
+        <Button variant="info" className="ms-3">Enviar</Button>
+        </Form.Group>
+      </Form>
+    </div>
+  );
 };
 
 export default FormularioTarea;
